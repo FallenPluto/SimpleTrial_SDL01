1a0f45c3830ab3da31ffe064b4cda067fa08aa49
FallenPluto <72704312+FallenPluto@users.noreply.github.com>
2020-12-25 17:05:06 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+    float y, a, b, c, d;
+
+    a = 3.56;
+    b = 1.02;
+    c = 3;
+    d = 2.43;
+
+    // The formula to fnd y 
+    y = pow(tan(a), 1 / c) / (1 + (sinh(b)) / (log(d + c)));
+
+    // Print the value of y 
+    cout << "The Solution is " << y;
+
+    return 0;
+}
