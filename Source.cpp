af9db79b26584168444b6f81d3921b59d3025995
FallenPluto <72704312+FallenPluto@users.noreply.github.com>
2020-12-28 14:58:18 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -12,7 +12,7 @@ int main()
     d = 2.43;
 
     // The formula to fnd y 
-    y = pow(tan(a), 1 / c) / (1 + (sinh(b)) / (log(d + c)));
+    y = pow(tan(a), 1 / c) / (1 + (sinh(b)) / (log(abs(d + c))));
 
     // Print the value of y 
     cout << "The Solution is " << y;
